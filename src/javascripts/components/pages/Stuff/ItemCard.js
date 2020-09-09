737bc2cc8f5bd9c43045cfec52084aba41f29203
Jim Browning <jamalebrowning@gmail.com>
2020-09-09 11:32:52 -0500

    delete item from stuff, single item, and firebase

--- src/javascripts/components/pages/Stuff/ItemCard.js
+++ src/javascripts/components/pages/Stuff/ItemCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle,
@@ -11,6 +13,13 @@ import itemShape from '../../../helpers/props/itemShape';
 class ItemCard extends React.Component {
   static propTypes = {
     item: itemShape.itemShape,
+    deleteItem: PropTypes.func.isRequired,
+  }
+
+  deleteItemEvent = (e) => {
+    e.preventDefault();
+    const { item, deleteItem } = this.props;
+    deleteItem(item.id);
   }
 
   render() {
@@ -26,8 +35,9 @@ class ItemCard extends React.Component {
         <CardBody>
           <CardTitle>{item.itemName}</CardTitle>
           <CardText>{item.itemDescription}</CardText>
-          <Link to={singleItemLink} className="btn btn-warning"><i className="fas fa-binoculars"></i></Link>
-          <Link to={editLink} className="btn btn-success"><i className="fas fa-pencil-alt"></i></Link>
+          <Link to={singleItemLink} className="btn btn-warning m-3"><i className="fas fa-binoculars"></i></Link>
+          <Link to={editLink} className="btn btn-success m-3"><i className="fas fa-pencil-alt"></i></Link>
+          <button className="btn btn-secondary m-3" onClick={this.deleteItemEvent}><i className="fas fa-trash-alt"></i></button>
         </CardBody>
       </Card>
     </div>
--- src/javascripts/components/pages/Stuff/Single.js
+++ src/javascripts/components/pages/Stuff/Single.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import itemsData from '../../../helpers/data/itemData';
 
 class Single extends React.Component {
@@ -14,9 +16,18 @@ class Single extends React.Component {
       .catch((err) => console.error('single item broke', err));
   }
 
+  deleteItem = () => {
+    const { itemId } = this.props.match.params;
+    itemsData.deleteItem(itemId)
+      .then(() => {
+        this.props.history.push('/stuff');
+      })
+      .catch((err) => console.error('delete single item sucks', err));
+  }
+
   render() {
     const { item } = this.state;
-    // const { itemId } = this.props.match.params;
+    const { itemId } = this.props.match.params;
     return (
       <div className="Single">
         <div className="SingleStuff">
@@ -26,7 +37,7 @@ class Single extends React.Component {
           <div className="card-body">
           <h5 className="card-title">{item.itemName}</h5>
           <p>{item.itemDescription}</p>
-          {/* <Link to={`/edit/${itemId}`} className="btn btn-secondary m-3"><i className="fas fa-pencil-alt"></i></Link> */}
+          <Link to={`/edit/${itemId}`} className="btn btn-secondary m-3"><i className="fas fa-pencil-alt"></i></Link>
           <button className="btn btn-secondary m-3" onClick={this.deleteItem}><i className="fas fa-trash-alt"></i></button>
         </div>
       </div>
